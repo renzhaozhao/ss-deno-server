149ea1448120696c24c93e4b576434575d7e11a0
renzhao <renzhao@vpgame.cn>
2020-06-09 15:55:36 +0800

    feat: 修改部署脚本

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 定义变量
+IMAGE_NAME='deno-image'
+CONTAINER_NAME='deno-server'
+
+# 构建docker镜像
+docker build -t $IMAGE_NAME .
+
+# 删除dokcer容器
+cid=$(docker ps -a | grep $CONTAINER_NAME |awk '{print $1}')
+if [ x"$cid" != x ] 
+then
+  docker rm -f $cid
+fi
+
+# 启动Docker容器
+docker run -d -p 80:3000 --name $CONTAINER_NAME $IMAGE_NAME
