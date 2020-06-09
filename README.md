c4e9d83869270369b74bcb6bf907a511e046b363
renzhao <renzhao@vpgame.cn>
2020-06-09 16:21:12 +0800

    feat: fix

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## 打包镜像
 ```shell
-$ docker build . -t deno-server:1.0.0
+$ docker build . -t deno-image:1.0.0
 ```
 
 ## 运行服务
 ```shell
-$ docker run -p 80:3000 -it --name deno-server deno-server:1.0.0
+$ docker run -p 80:3000 -it --name deno-server deno-image:1.0.0
 ```
--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ IMAGE_NAME='deno-image'
 CONTAINER_NAME='deno-server'
 
 # 构建docker镜像
-docker build -t $IMAGE_NAME .
+docker build . -t $IMAGE_NAME:1.0.0 
 
 # 删除dokcer容器
 cid=$(docker ps -a | grep $CONTAINER_NAME |awk '{print $1}')
@@ -15,4 +15,4 @@ then
 fi
 
 # 启动Docker容器
-docker run -d -p 80:3000 --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -p 80:3000 --name $CONTAINER_NAME $IMAGE_NAME:1.0.0 
